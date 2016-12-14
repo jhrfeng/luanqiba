cc387ea06264157286f8f8f5554250ec371953dc
lsj19930625 <lsj19930625@163.com>
2016-12-14 18:13:45 +0800

    更新历史行情

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Platform } from 'ionic-angular';
 import { ItemDetailsPage } from '../item-details/item-details';
 
 @Component({
@@ -7,27 +7,13 @@ import { ItemDetailsPage } from '../item-details/item-details';
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  pet: string = "listInfo";
+  isAndroid: boolean = true;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  constructor(public navCtrl: NavController, public navParams: NavParams, platform: Platform) {
+    //this.isAndroid = platform.is('android');
   }
-
+  
   itemTapped(event, item) {
     this.navCtrl.push(ItemDetailsPage, {
       item: item
