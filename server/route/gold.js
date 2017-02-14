9fb33191efc171003b21dad135a0d8a02c079682
jhrfeng <jhrfeng@163.com>
2017-02-14 18:13:50 +0800

    gold

--- server/route/gold.js
+++ server/route/gold.js
@@ -1,13 +1,62 @@
 var request = require('request');
 var schedule = require('node-schedule');
 var ali = require('../utils/ali');
+var redisClient = require('../config/redis_database').redisClient;
 
-var currentDate = new Date()
+var newDate = new Date()
+
+function sheduleOpt(){
+  var date = new Date().getDay();
+  if(date==0 || date==6)return false;
+
+  var hour = newDate.getHours();
+  if(hour>9 && hour<19)return true;
+    else return false;
+}
+
+
+function GetDateStr(count) { 
+  var newDate = new Date();
+  newDate.setDate(newDate.getDate()+count);//获取AddDayCount天后的日期 
+  var y = newDate.getFullYear(); 
+  var m = newDate.getMonth()+1;//获取当前月份的日期 
+  m = m < 10 ? '0' + m : m;  
+  var d = newDate.getDate();  
+  d = d < 10 ? ('0' + d) : d;
+  y = ''+y+m+d;
+  return Number(y); 
+} 
+
+// function save 
+// 存在set里面
 
 function scheduleCronstyle(){
-    schedule.scheduleJob('3 * * * * *', function(){
-        if(currentDate.getHours()>9 && currentDate.getHours()<16){
-          console.log('scheduleCronstyle:' + currentDate.getHours());
+    redisClient.lindex('money:price',GetDateStr(-1),function(err,result){
+       console.log(result);
+    })
+    schedule.scheduleJob('20 * * * * *', function(){
+        if(sheduleOpt()){
+          console.log('scheduleCronstyle:' + GetDateStr(-10));
+          var resultGold, resultMoney;
+          // 获取美元指数
+          request('http://web.juhe.cn:8080/finance/exchange/frate?type=&key=56070babcb424918dc9d62e3d276e432', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              resultMoney = JSON.parse(body);
+              resultMoney = resultMoney.result[0]["data2"];
+              redisClient.set('money', JSON.stringify(resultMoney));
+              // console.log(resultMoney.yesPic)
+              redisClient.zadd('money:price', GetDateStr(-1), resultMoney.yesPic);
+            }
+          });
+          //获取黄金价格
+          request('http://web.juhe.cn:8080/finance/gold/shgold?v=&key=03d9124bc4287ec2bc732c55679ad705', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              resultGold = JSON.parse(body);
+              resultGold = resultGold.result[0][4];
+              redisClient.set('gold', JSON.stringify(resultGold));
+              redisClient.zadd('gold:price', GetDateStr(-1), resultGold.yespri);
+            }
+          });
         }
     }); 
 }
@@ -15,19 +64,23 @@ function scheduleCronstyle(){
 scheduleCronstyle();
 
 exports.usaMoney = function(req, res){
-	request('http://web.juhe.cn:8080/finance/exchange/frate?type=&key=56070babcb424918dc9d62e3d276e432', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-      	res.send(body);
-      }
-    })
+  redisClient.get('money', function (err, reply) {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(reply);
+  });
 }
 
 exports.auGold = function(req, res){
-	request('http://web.juhe.cn:8080/finance/gold/shgold?v=&key=03d9124bc4287ec2bc732c55679ad705', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-      	res.send(body);
-      }
-    })
+  redisClient.get('gold', function (err, reply) {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(reply);
+  });
 }
 
 exports.uploadImg = function(req, res){
